db6e3ca3f0a920c4544bd16833350e7e47d6d251
tvanbesi <tvanbesi@gmail.com>
2021-07-08 12:50:26 +0200

    die condition done

--- main.c
+++ main.c
@@ -14,11 +14,24 @@
 #include "philo.h"
 
 static int
+	done_eating(t_philosopher **philosopher)
+{
+	while (*philosopher)
+	{
+		if ((*philosopher)->n_eat != 0)
+			return (0);
+		philosopher++;
+	}
+	return (1);
+}
+
+static int
 	init(t_philosopher **philosopher, int argc, char **argv, pthread_mutex_t **fork, pthread_mutex_t *wlock)
 {
 	int	n_philo;
 
 	n_philo = ft_atoi(argv[1]);
+	philosopher[n_philo] = NULL;
 	while (n_philo-- > 0)
 	{
 		if (!(philosopher[n_philo] = malloc(sizeof(**philosopher))))
@@ -43,12 +56,62 @@ static int
 }
 
 static void
-	*death(void *arg)
+	*monitor_thread(void *arg)
 {
 	t_philosopher	**philosopher;
+	struct timeval	time;
+	int				anydead;
 
 	philosopher = (t_philosopher**)arg;
-	
+	pthread_mutex_lock(philosopher[0]->wlock);
+	printf("Initializing\n");
+	gettimeofday(&time, NULL);
+	while (*philosopher)
+	{
+		(*philosopher)->tse_sec = time.tv_sec;
+		(*philosopher)->tse_usec = time.tv_usec;
+		philosopher++;
+	}
+	philosopher = (t_philosopher**)arg;
+	pthread_mutex_unlock(philosopher[0]->wlock);
+	anydead = 0;
+	while (!anydead && !done_eating(philosopher))
+	{
+		while (*philosopher)
+		{
+			gettimeofday(&time, NULL);
+			if (time.tv_sec - (*philosopher)->tse_sec > (*philosopher)->ttd / 1000)
+			{
+				if (time.tv_usec - (*philosopher)->tse_usec > (*philosopher)->ttd - ((time.tv_sec - (*philosopher)->tse_sec) * 1000))
+				{
+					(*philosopher)->status = DEAD;
+					pthread_mutex_lock((*philosopher)->wlock);
+					gettimeofday(&time, NULL);
+					printf("%ld+%ld Philosopher %d had died\n", time.tv_sec % 100, time.tv_usec / 1000, (*philosopher)->id);
+					pthread_mutex_unlock((*philosopher)->wlock);
+				}
+			}
+			else if (time.tv_sec - (*philosopher)->tse_sec == (*philosopher)->ttd / 1000)
+			{
+				if (time.tv_usec - (*philosopher)->tse_usec > (*philosopher)->ttd)
+				{
+					(*philosopher)->status = DEAD;
+					pthread_mutex_lock((*philosopher)->wlock);
+					gettimeofday(&time, NULL);
+					printf("%ld+%ld Philosopher %d had died\n", time.tv_sec % 100, time.tv_usec / 1000, (*philosopher)->id);
+					pthread_mutex_unlock((*philosopher)->wlock);
+				}
+			}
+			if ((*philosopher)->status == DEAD)
+			{
+				anydead = 1;
+				break ;
+			}
+			philosopher++;
+		}
+		philosopher = (t_philosopher**)arg;
+	}
+	return (NULL);
 }
 
 static void
@@ -58,7 +121,7 @@ static void
 	struct timeval	time;
 
 	philosopher = (t_philosopher*)arg;
-	while (philosopher->n_eat != 0)
+	while (philosopher->status == ALIVE)
 	{
 		pthread_mutex_lock(philosopher->rfork);
 		pthread_mutex_lock(philosopher->lfork);
@@ -66,20 +129,24 @@ static void
 		gettimeofday(&time, NULL);
 		philosopher->tse_sec = time.tv_sec;
 		philosopher->tse_usec = time.tv_usec;
-		printf("%ld+%ld Philosopher %d is eating\n", time.tv_sec % 100, time.tv_usec / 1000, philosopher->id);
+		if (philosopher->status == ALIVE)
+			printf("%ld+%ld Philosopher %d is eating\n", time.tv_sec % 100, time.tv_usec / 1000, philosopher->id);
 		pthread_mutex_unlock(philosopher->wlock);
-		philosopher->n_eat--;
+		if (philosopher->n_eat > 0)
+			philosopher->n_eat--;
 		usleep(philosopher->tte * 1000);
 		pthread_mutex_unlock(philosopher->rfork);
 		pthread_mutex_unlock(philosopher->lfork);
 		pthread_mutex_lock(philosopher->wlock);
 		gettimeofday(&time, NULL);
-		printf("%ld+%ld Philosopher %d is sleeping\n", time.tv_sec % 100, time.tv_usec / 1000, philosopher->id);
+		if (philosopher->status == ALIVE)
+			printf("%ld+%ld Philosopher %d is sleeping\n", time.tv_sec % 100, time.tv_usec / 1000, philosopher->id);
 		pthread_mutex_unlock(philosopher->wlock);
 		usleep(philosopher->tts * 1000);
 		pthread_mutex_lock(philosopher->wlock);
 		gettimeofday(&time, NULL);
-		printf("%ld+%ld Philosopher %d is thinking\n", time.tv_sec % 100, time.tv_usec / 1000, philosopher->id);
+		if (philosopher->status == ALIVE)
+			printf("%ld+%ld Philosopher %d is thinking\n", time.tv_sec % 100, time.tv_usec / 1000, philosopher->id);
 		pthread_mutex_unlock(philosopher->wlock);
 	}
 	return (NULL);
@@ -90,7 +157,7 @@ int
 {
 	t_philosopher		**philosopher;
 	pthread_t			*tid;
-	pthread_t			death;
+	pthread_t			monitor;
 	pthread_mutex_t		**fork;
 	pthread_mutex_t		wlock;
 	int					n_philo;
@@ -99,7 +166,7 @@ int
 	if (argc < 5 || argc > 6)
 		return (-1);
 	n_philo = ft_atoi(argv[1]);
-	if (!(philosopher = malloc(sizeof(*philosopher) * n_philo)))
+	if (!(philosopher = malloc(sizeof(*philosopher) * (n_philo + 1))))
 		return (-1);
 	if (!(fork = malloc(sizeof(*fork) * n_philo)))
 		return (-1);
@@ -118,7 +185,7 @@ int
 	if (!(tid = malloc(sizeof(*tid) * n_philo)))
 		return (-1);
 	i = 0;
-	if (pthread_create(&death, NULL, &death, philosopher) != 0)
+	if (pthread_create(&monitor, NULL, &monitor_thread, philosopher) != 0)
 		return (-1);
 	while (i < n_philo)
 	{
@@ -126,10 +193,7 @@ int
 			return (-1);
 		i++;
 	}
-	i = 0;
-	while (i < n_philo)
-		pthread_join(tid[i++], NULL);
-	printf("All philosophers are done eating\n");
+	pthread_join(monitor, NULL);
 	i = 0;
 	while (i < n_philo)
 		pthread_mutex_destroy(fork[i++]);
